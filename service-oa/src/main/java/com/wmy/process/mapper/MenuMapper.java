5b6cf7765c658a6c012a1a9854bc1ccd764e6f9e
YmengYang <3421849869@qq.com>
2023-11-15 16:24:51 +0800

    公众号端审批

---
+++ service-oa/src/main/java/com/wmy/process/mapper/MenuMapper.java
@@ -0,0 +1,10 @@
+package com.wmy.process.mapper;
+
+import com.wmy.model.wechat.Menu;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MenuMapper extends BaseMapper<Menu> {
+
+}
